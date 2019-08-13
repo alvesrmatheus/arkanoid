85933fb774764eb96401a428316fd1cbd613d0f3
Matheus Alves <mr634580@gmail.com>
2019-08-13 10:32:35 -0300

    Projeto desenvolvido no curso Unity2D

---
+++ Assets/codigos/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	// Use this for initialization
+	public Vector2 velocidade;
+	void Start () {
+		Rigidbody2D rigibody = GetComponent<Rigidbody2D>();
+		GetComponent<Rigidbody2D>().AddForce(velocidade);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/codigos/player01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player01 : MonoBehaviour {
+	// Update is called once per frame
+	public float velocidade = 10.0f;
+	public float horizontalAxis;
+	public Rigidbody2D rigidbody;
+
+	void Start(){
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	void Update () {
+		horizontalAxis = Input.GetAxis ("Horizontal");
+		rigidbody.velocity = new Vector2 (velocidade * horizontalAxis, 0);
+	}
+}
